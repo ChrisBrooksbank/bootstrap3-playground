281742858e1cd08aba4be18ddc52f2763011bc1a
chris brooksbank <chrisbrooksbank@gmail.com>
2016-07-26 09:46:49 +0100

    input groups

--- site.js
+++ site.js
@@ -1,10 +1,18 @@
 (function () {
 	"use strict";
 
+    // Set the button text to what was selected on the dropdown
 	var $pickButton = $("#pickButton");
 	$("#reasonDropdown li a").on("click", function() {
 		var reason = $(this).text();
 		$pickButton.text(reason);
+
+		/* this hasnt put the selected value into the form data
+      	how to do that is up to us
+      	Common approach is to add a hidden field and set its value here
+   		*/
 	});
+
+
 
 })();
